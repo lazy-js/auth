3cf25f16f5d50392c21bd66d083bf6fe9346798d
Mahmoud Karsha <mahmood997310m@gmail.com>
2025-09-07 13:51:47 +0300

    add mocknotificationclient

---
+++ src/utils/MockNotificationClientSdk.ts
@@ -0,0 +1,12 @@
+import { INotificationClientSdk, SendEmailBody } from '../types';
+
+export class MockNotificationClientSdk implements INotificationClientSdk {
+  constructor() {}
+  async available(): Promise<boolean> {
+    return true;
+  }
+
+  async sendEmail(body: SendEmailBody): Promise<void> {
+    console.log(body);
+  }
+}
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './createRolesTree';
+export * from './MockNotificationClientSdk';
